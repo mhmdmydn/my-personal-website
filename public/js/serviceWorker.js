5ea1eda57d6eb717c35592ff8915e27cb73ff716
mhmdmydn <muhammadmayudin12@gmail.com>
2021-08-11 06:46:02 +0700

    Test PWA

---
+++ public/js/serviceWorker.js
@@ -0,0 +1,8 @@
+
+if("serviceWorker" in navigator){
+	navigator.serviceWorker.register("sw.js")
+		.then(registration => {
+			//console.log("PWA Success");
+		})
+		.catch(error => {console.log(error);})
+}
